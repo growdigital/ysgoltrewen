6142a292019e001ecadd0dfae4a8c1a4d2033c7f
Jake Rayson <jake@growdigital.org>
2016-03-14 22:29:59 +0000

    first batch of styling, which is nice

--- header.php
+++ header.php
@@ -13,18 +13,16 @@
 		<title><?php bloginfo( 'name' ); ?><?php if ( is_home() ) { } else { echo " : "; } ?><?php wp_title(''); ?></title>
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 		<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/dist/humans.txt">
-
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<div id="svg"></div><!-- SVG symbol icons ajax injection div -->
-		<header><!-- site-header -->
-			<div><!-- site-branding -->
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri (); ?>/dist/assets/img/logo.png" height="137" width="220" alt="Logo"></a></h1>
-					<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2><?php echo get_bloginfo('description');?></h2>
+		<header class="header"><!-- site-header -->
+			<div class="brand"><!-- site_branding -->
+					<a class="brand--logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri (); ?>/dist/assets/img/logo.svg" width="130" height="125" alt="Ysgol Trewen logo"></a></h1>
+					<h1 class="brand--name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="brand--desc"><?php echo get_bloginfo('description');?></h2>
 			</div>
-			<nav role="navigation">
+			<nav class="nav" role="navigation">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav>
 		</header>
