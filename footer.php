ba3ba54f0a844bdc2f4550347bccd095dab916e3
Jake Rayson <jake@growdigital.org>
2016-03-15 22:03:35 +0000

    basic page styling for mobile

--- footer.php
+++ footer.php
@@ -8,8 +8,6 @@
 ?>
 		</div><!-- /site-content -->
 		<footer role="contentinfo">
-			<?php wp_nav_menu( array( 'theme_location' => 'secondary', 'menu_id' => 'secondary-menu' ) ); ?>
-			<p>The <a href="https://github.com/growdigital/ysgoltrewen">ysgoltrewen theme</a> is licensed under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GPL v3</a> license, the <a href="https://github.com/growdigital/ysgoltrewen/blob/master/dist/assets/img/logo.png">ysgoltrewen logo</a> is copyright &copy; Grow Digital Ltd 2015. The content of this site is licensed under a <a href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 			<ul>
 				<li>
 					<a href="https://twitter.com/ygtrewen">
--- front-page.php
+++ front-page.php
@@ -7,8 +7,13 @@
 
 get_header(); ?>
 
-<main role="main">
-<h1>Home page</h1>
+<main class="main" role="main">
+	<?php while (have_posts()) : the_post(); ?>
+		<article <?php post_class(); ?>>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</article>
+	<?php endwhile; ?>
 </main>
 
 <?php
