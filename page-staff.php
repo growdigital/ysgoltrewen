03ce71ec82661c4230bdbdaf30ea8b2465acb2bf
Jake Rayson <jake@growdigital.org>
2016-04-02 11:11:21 +0100

    basic media object styling on staff page

--- page-staff.php
+++ page-staff.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the staff page
  * @link https://codex.wordpress.org/Template_Hierarchy
  * @package ysgoltrewen
  */
@@ -13,12 +13,14 @@ get_header(); ?>
 		<h1><?php single_post_title(); ?></h1>
 		<?php
 			if( have_rows('member') ): ?>
-				<ul>
+				<ul class="zero">
 				<?php while ( have_rows('member') ) : the_row(); ?>
-					<li>
-						<h2><?php the_sub_field('name'); ?></h2>
-						<h3><?php the_sub_field('title'); ?></h3>
-						<img src="<?php the_sub_field('photo'); ?>" alt="Photograph of <?php the_sub_field('name'); ?>" />
+					<li class="media media--list">
+						<div class="media__body">
+							<img class="media__img" width="20%" src="<?php the_sub_field('photo'); ?>" alt="Photograph of <?php the_sub_field('name'); ?>" />
+							<h2><?php the_sub_field('name'); ?></h2>
+							<h3><?php the_sub_field('title'); ?></h3>
+						</div>
 					</li>
 				<?php	endwhile; ?>
 				</ul>
