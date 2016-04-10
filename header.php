4a607ca2fb11902dc3ce0e5e32bb49f39163167a
Jake Rayson <jake@growdigital.org>
2016-04-10 20:12:15 +0100

    header styling. deflist styling. added flexbox to modernzir

--- header.php
+++ header.php
@@ -19,10 +19,10 @@
 	</head>
 	<body <?php body_class(); ?>>
 		<header class="header"><!-- site-header -->
-			<a class="header--logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri (); ?>/dist/assets/img/logo.svg" alt="Ysgol Trewen logo"></a></h1>
+			<a class="header__logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri (); ?>/dist/assets/img/logo.svg" alt="Ysgol Trewen logo"></a></h1>
 			<div>
-				<h1 class="header--name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="header--desc"><?php echo html_entity_decode(get_bloginfo('description'));?></h2>
+				<h1 class="header__name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="header__desc"><?php echo html_entity_decode(get_bloginfo('description'));?></h2>
 			</div>
 		</header>
 		<nav class="nav" role="navigation">
