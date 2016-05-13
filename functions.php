3c555a443d3bfcc005cf1087b4d2c572138a0db4
Jake Rayson <jake@growdigital.org>
2016-05-13 20:28:12 +0100

    made mu plugin for school details widget, moved layout css temporarily to shame.css, commented out staff images for now

--- functions.php
+++ functions.php
@@ -99,8 +99,6 @@ add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
-
-
 /**
  * Add ACF global page
  */
@@ -150,51 +148,6 @@ if( function_exists('acf_add_options_page') ) {
 
 
 }
-
-/**
- * Custom School details widget using info from ACF options page
- * http://codex.wordpress.org/Widgets_API
- */
-
-class school_widget extends WP_Widget {
-
-	/**
-	 * Setup widget name & description
-	 */
-	public function __construct() {
-		$widget_ops = array(
-			'classname' => 'school_widget',
-			'description' => 'Displays ACF global school details',
-		);
-		parent::__construct( 'school_widget', 'School details', $widget_ops );
-	}
-
-	/**
-	 * Outputs widget content
-	 */
-	public function widget( $args, $instance ) {
-		echo '<h2>' , _e( 'School details', 'ysgoltrewen' ) , '</h2>';
-		echo the_field('address', 'option');
-		echo '<dl class="deflist">';
-		echo '<dt>' , _e( 'Telephone', 'ysgoltrewen' )  , ':</dt>';
-		echo '<dd><a href="', the_field('tel_no', 'option') , '">' , the_field('tel_display', 'option') , '</a></dd><br>';
-		echo '<dt>' , _e( 'Email', 'ysgoltrewen' ) , ':</dt>';
-		echo '<dd><a href="mailto:', the_field('email', 'option') , '">' , the_field('email', 'option') , '</a></dd><br>';
-		echo '<dt>' , _e( 'Headteacher', 'ysgoltrewen' ) , ':</dt>';
-		echo '<dd>' , the_field('head_teacher', 'option') , '</dd><br>';
-		echo '<dt>' , _e( 'Language', 'ysgoltrewen' ) , ':</dt>';
-		echo '<dd>' , the_field('language', 'option') , '</dd><br>';
-		echo '<dt>' , _e( 'Age range', 'ysgoltrewen' ) , ':</dt>';
-		echo '<dd>' , the_field('age_range', 'option') , '</dd><br>';
-		echo '<dt>' , _e( 'Number of pupils', 'ysgoltrewen' ) , ': </dt>';
-		echo '<dd>' , the_field('number_pupils', 'option') , '</dd><br>';
-		echo '</dl>';
-	}
-}
-
-add_action( 'widgets_init', function(){
-	register_widget( 'school_widget' );
-});
 
 /**
  * Enqueue scripts and styles.
